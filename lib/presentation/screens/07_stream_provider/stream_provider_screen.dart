edd1b4f595d71550c29a92754ace540cf7be96cd
Gilvan Costa <gilvancosta2025@gmail.com>
2023-11-19 10:36:12 -0300

    stream_provider

--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'providers/stream_provider.dart';
+
 
 
 class StreamProviderScreen extends StatelessWidget {
@@ -10,6 +14,42 @@ class StreamProviderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stream Provider'),
       ),
+      body: const StreamView(),
+    );
+  }
+}
+
+
+class StreamView extends ConsumerWidget {
+  const StreamView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final usersInChatAsync = ref.watch(usersInChatProvider);
+
+    if ( usersInChatAsync.isLoading ) {
+      return const CircularProgressIndicator();
+    }
+
+    if ( usersInChatAsync.hasError ) {
+      return Center(
+        child: Text('${usersInChatAsync.error}'),
+      );
+    }
+
+    final users = usersInChatAsync.value!;
+
+    return ListView.builder(
+      itemCount: users.length,
+      itemBuilder: (BuildContext context, int index) {
+
+        final user = users[index];
+
+        return ListTile(
+          title: Text( user ),
+        );
+      },
     );
   }
 }
